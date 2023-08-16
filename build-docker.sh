259b4a77a14cae6847d8af3a7cd2a693527bc53e
aerielabio <dev@aerielab.io>
2023-08-16 19:15:26 +0700

    fixes

--- build-docker.sh
+++ build-docker.sh
@@ -1,1 +1,2 @@
-docker build -t aerielab/lighthouse:latest -t aerielab/lighthouse:v1.1 --build-arg FEATURES=aerie,jemalloc .
+#docker build -t aerielab/lighthouse:dev --build-arg FEATURES=aerie,jemalloc .
+docker build -t aerielab/lighthouse:latest -t aerielab/lighthouse:v1.3 --build-arg FEATURES=aerie,jemalloc .
